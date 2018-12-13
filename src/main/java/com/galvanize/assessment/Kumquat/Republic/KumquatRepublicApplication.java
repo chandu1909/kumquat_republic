01134729ee0ff0c37d32f43ad033025c6dd06adf
chandu1909 <chandra.polavarapu516@topper.wku.edu>
2018-12-13 13:36:38 -0700

    initial commit

---
+++ src/main/java/com/galvanize/assessment/Kumquat/Republic/KumquatRepublicApplication.java
@@ -0,0 +1,14 @@
+package com.galvanize.assessment.Kumquat.Republic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KumquatRepublicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KumquatRepublicApplication.class, args);
+	}
+
+}
+
